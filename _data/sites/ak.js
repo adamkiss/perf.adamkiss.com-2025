7b2c480078a0a8f10b0792832e9994f0666afdd6
Adam Kiss <iam@adamkiss.com>
2025-02-03 13:49:56 +0100

    Update BT24 to BT25

--- _data/sites/ak.js
+++ _data/sites/ak.js
@@ -1,20 +1,20 @@
 module.exports = {
-	description: "AK / BS Sites",
-	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
-		// Use "run" if the sites don’t share assets on the same origin
-		//           and we can reset chrome with each run instead of
-		//           each site in every run (it’s faster)
-		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "run",
-	},
-	urls: [
-		"https://adamkiss.com/",
+    description: "AK / BS Sites",
+    options: {
+        frequency: 60 * 11 + 30, // 11h, 30m
+        // Use "run" if the sites don’t share assets on the same origin
+        //           and we can reset chrome with each run instead of
+        //           each site in every run (it’s faster)
+        // Use "site" if sites are all on the same origin and share assets.
+        freshChrome: "run",
+    },
+    urls: [
+        "https://adamkiss.com/",
         "https://bestshot.sk/",
-		"https://rsvp.sk/",
-		"https://2024.besttour.sk/",
-		"https://chcemvino.sk/",
-		"https://chcemvinonaevent.sk/",
+        "https://rsvp.sk/",
+        "https://chcemvino.sk/",
+        "https://2025.besttour.sk/",
+        "https://chcemvinonaevent.sk/",
         "https://matrix-printer.adamkiss.com/",
-	]
+    ]
 };
